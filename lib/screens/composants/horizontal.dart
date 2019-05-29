c6bd3c9804fa473287d9cfcc478f9064555dcb11
Asam237 <abbasalimokolo@gmail.com>
2019-05-29 10:45:01 +0100

    index

--- lib/screens/composants/horizontal.dart
+++ lib/screens/composants/horizontal.dart
@@ -4,34 +4,31 @@ class Horizontal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 88.0,
+      height: 150.0,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
           Categorie(
-            titre: "images/cats/dress.png",
-            sousTitre: "Test",
-          ),
-          Categorie(
             titre: "images/cats/accessories.png",
-            sousTitre: "Test",
+            sousTitre: "Accessoires",
           ),
           Categorie(
-            titre: "images/cats/formal.png",
-            sousTitre: "Test",
+            titre: "images/cats/tel.png",
+            sousTitre: "Téléphones",
           ),
           Categorie(
-            titre: "images/cats/dress.png",
-            sousTitre: "Test",
+            titre: "images/cats/chaussure.png",
+            sousTitre: "Chaussure",
           ),
           Categorie(
-            titre: "images/cats/accessories.png",
-            sousTitre: "Test",
+            titre: "images/cats/boisson.png",
+            sousTitre: "Boisson",
           ),
           Categorie(
-            titre: "images/cats/formal.png",
-            sousTitre: "Test",
+            titre: "images/cats/dd.png",
+            sousTitre: "Disque dur",
           ),
+
         ],
       ),
     );
@@ -46,7 +43,7 @@ class Categorie extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(5.0),
+        padding: EdgeInsets.all(2.0),
         child: InkWell(
             onTap: null,
             child: Container(
@@ -54,12 +51,11 @@ class Categorie extends StatelessWidget {
               child: ListTile(
                 title: Image.asset(
                   titre,
-                  width: 90.0,
+                  width: 100.0,
                   height: 80.0,
                 ),
                 subtitle: Container(
-                  alignment: Alignment.center,
-                  child: Text(sousTitre),
+                  child: Text(sousTitre, style: TextStyle(fontSize: 13.0, color: Colors.black), textAlign: TextAlign.center,),
                 ),
               ),
             )));
